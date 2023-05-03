803c78b5c0f98fb8467b2ad73795d4afc7055cee
ultron <mulerfrommarss@gmail.com>
2023-05-03 23:17:35 +0300

    70%

---
+++ assets/Information-24ffa42c.js
@@ -0,0 +1,1 @@
+import{_ as e,at as n,l as o}from"./index-7ecd0b76.js";const t={};function r(a,c){return o(),n("h1",null,"Information page")}const _=e(t,[["render",r]]);export{_ as default};
---
+++ assets/ssrBoot-5800807d.js
@@ -0,0 +1,1 @@
+import{r as t,T as e,c as s,bc as n}from"./index-7ecd0b76.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
